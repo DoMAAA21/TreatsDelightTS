f3ca6aaa05e5810ca145fa82d4edf27e08d87b93
DOMAA21 <clashecurry21@gmail.com>
2024-01-14 21:06:27 +0800

    add loader to product details

--- src/components/loaders/ProductDetailsLoader.tsx
+++ src/components/loaders/ProductDetailsLoader.tsx
@@ -1,28 +1,55 @@
 const ProductDetailsLoader = () => {
     return (
-        <div className="w-full h-full animate-pulse mt-8" >
-            <div className="hidden lg:flex md:flex mx-20 px-20" style={{height : '72vh'}}>
-                <div className="w-4/6 bg-gray-300 rounded-bl-2xl rounded-tl-2xl overflow-hidden shadow-md" >
-                    <div className="animate-pulse h-96"></div>
-                </div>
-
-                <div className="w-2/6 bg-gray-300 shadow-md p-4 text-justify flex flex-col rounded-tr-2xl rounded-br-2xl">
-                    <div className="animate-pulse flex-grow mt-20">
-                        <div className="h-6 bg-gray-200 mb-4 w-2/3 rounded-lg"/>
-                        <div className="h-8 bg-gray-200 mb-4 w-4/5 rounded-lg"/>
-                        <div className="h-6 bg-gray-200 mb-4 w-3/4 rounded-lg"/>
+        <>
+            <div className="w-full h-full animate-pulse mt-8" >
+                <div className="hidden lg:flex md:flex mx-20 px-20" style={{ height: '72vh' }}>
+                    <div className="w-4/6 bg-gray-300 rounded-bl-2xl rounded-tl-2xl overflow-hidden shadow-md" >
+                        <div className="animate-pulse h-96"></div>
                     </div>
 
-                    <div className="flex items-center mb-4">
-                        <div className="h-8 bg-gray-200 w-1/4" />
-                        <div className="relative text-center w-16 ml-2">
-                            <div className="h-8 bg-gray-200" />
+                    <div className="w-2/6 bg-gray-300 shadow-md p-4 text-justify flex flex-col rounded-tr-2xl rounded-br-2xl">
+                        <div className="animate-pulse flex-grow mt-20">
+                            <div className="h-6 bg-gray-200 mb-4 w-2/3 rounded-lg" />
+                            <div className="h-8 bg-gray-200 mb-4 w-4/5 rounded-lg" />
+                            <div className="h-6 bg-gray-200 mb-4 w-3/4 rounded-lg" />
                         </div>
+
+                        <div className="flex items-center mb-4">
+                            <div className="h-8 bg-gray-200 w-1/4" />
+                            <div className="relative text-center w-16 ml-2">
+                                <div className="h-8 bg-gray-200" />
+                            </div>
+                        </div>
+                        <div className="h-12 bg-gray-200 w-full rounded-lg" />
                     </div>
-                    <div className="h-12 bg-gray-200 w-full rounded-lg" />
                 </div>
             </div>
-        </div>
+            <div className="lg:hidden md:hidden">
+                <div className="animate-pulse">
+                    <div className="h-[200px] md:h-full w-full bg-gray-300 rounded" />
+                </div>
+            </div>
+            <div className="lg:hidden md:hidden">
+                <div className="animate-pulse">
+                    <div className="h-[200px] md:h-full w-full bg-gray-300 rounded p-2">
+                        <div className="animate-pulse flex-grow">
+                            <div className="h-6 bg-gray-200 mb-4 w-2/3 rounded-lg" />
+                            <div className="h-4 bg-gray-200 mb-4 w-4/5 rounded-lg" />
+                            <div className="h-6 bg-gray-200 mb-4 w-3/4 rounded-lg py-2" />
+                        </div>
+                        <div className="flex items-center mb-4">
+                            <div className="h-6 bg-gray-200 w-1/4" />
+                            <div className="relative text-center w-16 ml-2">
+                                <div className="h-6 bg-gray-200" />
+                            </div>
+                        </div>
+                        <div className="h-6 bg-gray-200 w-full rounded-lg" />
+                    </div>
+                </div>
+            </div>
+
+
+        </>
     );
 };
 
--- src/views/client/shop/productDetails.tsx
+++ src/views/client/shop/productDetails.tsx
@@ -31,7 +31,7 @@ const ProductDetails = () => {
   const settings = {
     // dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
@@ -44,35 +44,74 @@ const ProductDetails = () => {
   };
 
   return (
+    <>{fetchLoading ? <ProductDetailsLoader /> : (
+      <>
+        <div className="hidden lg:flex md:flex lg:mx-20 md:mx-8 md:px-8 lg:px-20 mt-8 h-full ">
+          <div className="w-4/6 rounded-bl-2xl rounded-tl-2xl overflow-hidden shadow-md lg:h-[600px]">
+            <Slider {...settings} useCSS>
+              {images.map((image, index) => (
+                <div key={index} className="lg:h-[600px] md:h-full w-full">
+                  <img
+                    src={image.url}
+                    alt={`Image ${index + 1}`}
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+              ))}
+            </Slider>
+          </div>
+          <div className="w-2/6 bg-white shadow-md p-4 text-justify flex flex-col rounded-tr-2xl rounded-br-2xl border border-gray-200">
+            <div className="flex-grow mt-20">
+              <h2 className="text-3xl font-bold mb-4">{product.name}</h2>
+              <p className="text-gray-600 text-xl mb-4">{product.description}</p>
+              <p className="text-green-500 text-xl font-semibold mb-4">₱{product?.sellPrice.toFixed(2)}</p>
+            </div>
+            <div className="flex items-center mb-4">
+              <label htmlFor="quantity" className="text-md text-gray-600 mr-2">
+                Quantity:
+              </label>
+              <div className="relative text-center w-16">
+                <input
+                  id="quantity"
+                  type="number"
+                  min="1"
+                  value={quantity}
+                  onChange={(e) => setQuantity(parseInt(e.target.value))}
+                  className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                />
+              </div>
+            </div>
+            <button
+              className={`${colors.danger} py-2 px-4 rounded-full w-full`}
+              onClick={handleAddToCart}
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
 
-    <>
-      <div className="hidden lg:flex md:flex lg:mx-20 md:mx-8 md:px-8 lg:px-20 mt-8 h-full ">
-        <div className="w-4/6 rounded-bl-2xl rounded-tl-2xl overflow-hidden shadow-md lg:h-[600px]">
-          <Slider {...settings} useCSS>
+
+        {/* Mobile View */}
+        <div className="lg:hidden md:hidden" >
+          <Slider {...settings}>
             {images.map((image, index) => (
-              <div key={index} className="lg:h-[600px] md:h-full w-full">
-              <img
-                src={image.url}
-                alt={`Image ${index + 1}`}
-                className="h-full w-full object-cover"
-              />
+              <div key={index} className="h-[200px] md:h-full w-full">
+                <img src={image?.url} alt={`Image ${index + 1}`} className="w-full h-full" />
               </div>
             ))}
           </Slider>
         </div>
-        <div className="w-2/6 bg-white shadow-md p-4 text-justify flex flex-col rounded-tr-2xl rounded-br-2xl border border-gray-200">
-          <div className="flex-grow mt-20">
-            <h2 className="text-3xl font-bold mb-4">{product.name}</h2>
-            <p className="text-gray-600 text-xl mb-4">{product.description}</p>
-            <p className="text-green-500 text-xl font-semibold mb-4">₱{product?.sellPrice.toFixed(2)}</p>
-          </div>
+        <div className="lg:hidden md:hidden mt-4 shadow-lg p-4 rounded-lg border-t border-gray-100">
+          <h2 className="text-2xl font-semibold mb-4">{product.name}</h2>
+          <p className="text-gray-600 mb-4">{product.description}</p>
+          <p className="text-gray-800 font-semibold mb-4">₱{product?.sellPrice.toFixed(2)}</p>
           <div className="flex items-center mb-4">
             <label htmlFor="quantity" className="text-md text-gray-600 mr-2">
               Quantity:
             </label>
             <div className="relative text-center w-16">
               <input
-                id="quantity"
+                id="mobile_quantity"
                 type="number"
                 min="1"
                 value={quantity}
@@ -81,6 +120,7 @@ const ProductDetails = () => {
               />
             </div>
           </div>
+
           <button
             className={`${colors.danger} py-2 px-4 rounded-full w-full`}
             onClick={handleAddToCart}
@@ -88,46 +128,8 @@ const ProductDetails = () => {
             Add to Cart
           </button>
         </div>
-      </div>
-
-
-      {/* Mobile View */}
-      <div className="lg:hidden md:hidden" >
-        <Slider {...settings}>
-          {images.map((image, index) => (
-            <div key={index} className="h-[200px] md:h-full w-full">
-            <img src={image?.url} alt={`Image ${index + 1}`} className="w-full h-full"/>
-            </div>
-          ))}
-        </Slider>
-      </div>
-      <div className="lg:hidden md:hidden mt-4 shadow-lg p-4 rounded-lg border-t border-gray-100">
-        <h2 className="text-2xl font-semibold mb-4">{product.name}</h2>
-        <p className="text-gray-600 mb-4">{product.description}</p>
-        <p className="text-gray-800 font-semibold mb-4">₱{product?.sellPrice.toFixed(2)}</p>
-        <div className="flex items-center mb-4">
-          <label htmlFor="quantity" className="text-md text-gray-600 mr-2">
-            Quantity:
-          </label>
-          <div className="relative text-center w-16">
-            <input
-              id="mobile_quantity"
-              type="number"
-              min="1"
-              value={quantity}
-              onChange={(e) => setQuantity(parseInt(e.target.value))}
-              className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-            />
-          </div>
-        </div>
-
-        <button
-          className={`${colors.danger} py-2 px-4 rounded-full w-full`}
-          onClick={handleAddToCart}
-        >
-          Add to Cart
-        </button>
-      </div>
+      </>
+    )}
     </>
 
 
