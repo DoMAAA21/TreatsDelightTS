889de66ca326982f450a68b74bf9174bda5e5649
DOMAA21 <clashecurry21@gmail.com>
2024-02-24 07:34:41 +0800

    add lazy loading

--- src/views/client/shop/productCard.tsx
+++ src/views/client/shop/productCard.tsx
@@ -30,7 +30,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ _id, name, sellPrice, images,
 
   return (
     <div className="max-w-md bg-white rounded-xl overflow-hidden shadow-lg mb-10 mx-4 transform transition-transform hover:scale-105">
-      <img className="w-full h-48 object-cover" src={images[0]?.url} alt={name} />
+      <img className="w-full h-48 object-cover" src={images[0]?.url} alt={name} loading="lazy" />
       <div className="p-4">
         <div className="text-xl font-semibold mb-2">{name}</div>
         <p className="text-gray-800 text-lg">{store?.name}</p>
