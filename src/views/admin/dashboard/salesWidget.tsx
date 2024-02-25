08ac9d31c70fcfe82ff9bd56414b78ba1b9e77ec
DOMAA21 <clashecurry21@gmail.com>
2024-02-25 14:57:58 +0800

    removed unneccesary codes and style changes

--- src/views/admin/dashboard/salesWidget.tsx
+++ src/views/admin/dashboard/salesWidget.tsx
@@ -16,28 +16,28 @@ const SalesWidget = () => {
         dispatch(fetchSalesToday());
     }, [dispatch]);
 
-    const expenses = [
+    const sales = [
         { title: 'Sales This Month', qty: salesThisMonth, icon: PesoIcon },
         { title: 'Sales Today', qty: salesToday, icon: MoneyIcon, },
     ];
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-            {expenses.map((exp, index) => (
+            {sales.map((sales, index) => (
                 <div
                     key={index}
                     className="bg-white lg:p-18 md:p-16 p-4 rounded-md shadow-md cursor-pointer hover:bg-gray-300 flex"
                 >
                     <div className="bg-indigo-400 p-4 w-24 h-24 rounded-full mr-4 flex-shrink-0 transition-transform hover:scale-110">
-                        <img className="w-16 h-16 pb-2" src={exp.icon} alt="widget logo" />
+                        <img className="w-16 h-16 pb-2" src={sales.icon} alt="widget logo" />
                     </div>
 
                     <div className="flex flex-col items-center">
                         <h2 className="text-lg font-semibold">
-                            <span className="text-3xl font-bold mb-2" style={{ color: (+exp.qty || 0) < 0 ? 'red' : 'green' }}>
-                                {((+exp.qty || 0) < 0 ? `-₱${Math.abs(+exp.qty || 0)}` : `₱${+exp.qty || 0}`)}
+                            <span className="text-3xl font-bold mb-2 text-green-700">
+                                {`₱${sales.qty ?? 0}`}
                             </span>
-                            <p> {exp.title}</p>
+                            <p className="text-lg font-semibold"> {sales.title}</p>
                         </h2>
                     </div>
                 </div>
