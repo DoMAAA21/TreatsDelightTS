a069c0ddf63787bdbec12e08a2aea55f01052d76
DOMAA21 <clashecurry21@gmail.com>
2024-02-25 09:54:20 +0800

    add dashboard link on popup

--- src/layouts/defaultLayout/navbar.tsx
+++ src/layouts/defaultLayout/navbar.tsx
@@ -181,6 +181,9 @@ const Navbar: React.FC = () => {
                     <li className="mb-2">
                       <a className="hover:text-indigo-500 cursor-pointer">Profile</a>
                     </li>
+                    <li className="mb-2">
+                      <a className="hover:text-indigo-500 cursor-pointer" onClick={()=>{navigate('/admin')}} >Dashboard</a>
+                    </li>
                     <li>
                       <a className="hover:text-indigo-500 cursor-pointer" onClick={logoutHandler}>Logout</a>
                     </li>
