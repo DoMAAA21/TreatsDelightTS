a77645e92ca6aaaeac854b5040930e1698da9f9f
DOMAA21 <clashecurry21@gmail.com>
2024-03-07 08:46:49 +0800

    style change

--- src/layouts/defaultLayout/navbar.tsx
+++ src/layouts/defaultLayout/navbar.tsx
@@ -89,7 +89,7 @@ const Navbar: React.FC = () => {
     <>
       <header>
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2 border">
-          <div className="flex flex-wrap justify-between items-center  max-w-screen-xl">
+          <div className="flex flex-wrap items-center  max-w-screen-xl">
 
             <Link to="/home" className="flex items-center">
               <img src={Logo} className="h-20 sm:h-16" alt="Logo" />
@@ -140,47 +140,51 @@ const Navbar: React.FC = () => {
 
             </div>
             <div
-              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+              className="hidden items-center w-full lg:flex lg:w-auto lg:order-1"
               id="mobile-menu-2"
             >
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-2 lg:mt-0">
-
-
+              <ul className="pl-10 flex flex-col mt-4 font-medium lg:flex-row lg:space-x-2 lg:mt-0">
                 {navConfig.map((item) => (
                   <li key={item.title}>
                     <Link
                       to={item.path}
-                      className={`block py-2 pr-4 pl-4 text-xl ${isLinkActive(item.path) ? 'text-violet-700' : 'text-gray-700'}
-                      } hover:text-violet-700`}
+                      className={`block py-2 pr-4 pl-4 text-xl ${isLinkActive(item.path) ? 'text-sky-500' : 'text-black'}
+                      } hover:text-sky-500`}
                     >
                       {item.title}
                     </Link>
                   </li>
-
                 ))}
               </ul>
-
-
             </div>
-
           </div>
 
 
 
           <div className="absolute hidden lg:flex right-3 top-2 h-16  items-center z-10" ref={dropdownRef}>
             <div className="relative">
-              <button
-                onClick={toggleDropdown}
-                type="button"
-                className="h-10 w-10 rounded-full overflow-hidden focus:outline-none"
-              >
-                {isAuthenticated ? (<img src={user?.avatar?.url} alt="User Avatar" className="h-full w-full object-cover" />) : null}
-              </button>
+              {isAuthenticated ? (
+                <button
+                  onClick={toggleDropdown}
+                  type="button"
+                  className="h-10 w-10 rounded-full overflow-hidden focus:outline-none"
+                >
+                  <img src={user?.avatar?.url} alt="User Avatar" className="h-full w-full object-cover" />
+
+                </button>
+
+              ) :
+                <div className="bg-indigo-500 rounded-lg px-3 py-2">
+                  <a className="hover:text-indigo-500 cursor-pointer my-3 font-medium text-md text-white" onClick={() => { navigate('/login') }} >Login</a>
+                </div>
+              }
               {isOptionsOpen && (
                 <div className="absolute top-full w-32 right-0 bg-white border border-gray-200 rounded-lg shadow-lg">
                   <ul className="py-2 text-center">
                     <li className="mb-2">
-                      <a className="hover:text-indigo-500 cursor-pointer">Profile</a>
+                      <Link to="/admin/profile" className="hover:text-indigo-500 cursor-pointer">
+                        Profile
+                      </Link>
                     </li>
                     {user && allowedOnAdminRoles.includes(user.role.toLowerCase())
                       &&
