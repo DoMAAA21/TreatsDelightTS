1127ba79e8607c77d2e998601622af2f8828a622
DOMAA21 <clashecurry21@gmail.com>
2024-03-09 09:48:16 +0800

    added error handling

--- src/views/error/restricted.tsx
+++ src/views/error/restricted.tsx
@@ -7,7 +7,7 @@ const RestrictedPage = () => {
       <MetaData title={'Restricted'}/>
       <div className="min-h-screen flex flex-col justify-center items-center">
         <div className="text-center">
-            <img className="w-full h-full" src={RestrictedIcon} alt="RestrictedIcon" />
+            <img src={RestrictedIcon} alt="RestrictedIcon" style={{height: '500px'}} />
           <h1 className="text-3xl font-semibold text-yellow-500 mb-4">Restricted Page</h1>
           <p className="text-lg mb-8">This is a restricted page. Only authorize users can view this content.</p>
           <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
