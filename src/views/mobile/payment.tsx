cede235ce451cd2cb7819dd2471c7c54520a392f
DOMAA21 <clashecurry21@gmail.com>
2024-03-12 14:21:51 +0800

    style changes

--- src/views/mobile/payment.tsx
+++ src/views/mobile/payment.tsx
@@ -7,15 +7,14 @@ const PaymentPage: React.FC = () => {
     return (
         <>
             <MetaData title={'Payment'} />
-            <div className="flex justify-center items-center pt-40 p-20">
+            <div className="flex justify-center items-center pt-40 py-20 px-4">
                 <div className="bg-white p-4 md:p-8 rounded-lg shadow-md h-60 w-full md:w-1/2 lg:w-1/3 pb-20">
                     <div className="mt-6">
-                        <h2 className="text-2xl text-center mb-4">Pay With</h2>
+                        <h2 className="text-2xl text-center font-semibold mb-4">Pay with</h2>
                         <div className="space-y-4">
                             <PaypalCheckoutButton />
                         </div>
                     </div>
-                    <input type="hidden" id="totalPrice" ></input>
                 </div>
             </div>
         </>
