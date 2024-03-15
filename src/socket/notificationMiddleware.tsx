2de9d1f9060279b4d5de70e65a62b50030138c96
DOMAA21 <clashecurry21@gmail.com>
2024-03-15 10:21:15 +0800

    removed unncessary

--- src/socket/notificationMiddleware.tsx
+++ src/socket/notificationMiddleware.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import io from "socket.io-client";
-import { dangerNotificationMsg, neutralNotificationMsg } from "../components/toast";
+import { neutralNotificationMsg } from "../components/toast";
 import { useAppSelector } from "../hooks";
 import NotificationPopSound from "../assets/sounds/notification-pop.mp3";
-import BellSound from "../assets/sounds/bell.mp3";
+// import BellSound from "../assets/sounds/bell.mp3";
 
 const NotificationMiddleware = () => {
     const socket = io(import.meta.env.VITE_BASE_URL, { transports: ["websocket"] });
@@ -14,23 +14,18 @@ const NotificationMiddleware = () => {
         });
 
 
-        socket.on("new_user_login", (data) => {
-            neutralNotificationMsg(data.message);
-            const audio = new Audio(NotificationPopSound);
-            audio.play();
-        });
+        // socket.on("new_user_login", (data) => {
+        //     neutralNotificationMsg(data.message);
+        //     const audio = new Audio(NotificationPopSound);
+        //     audio.play();
+        // });
 
         socket.on(`notification/${user?._id}`, (data) => {
-            dangerNotificationMsg(data.message);
+            neutralNotificationMsg(data.message);
             const audio = new Audio(NotificationPopSound);
             audio.play();
         });
 
-        socket.on(`danger/${user?._id}`, (data) => {
-            neutralNotificationMsg(data.message);
-            const audio = new Audio(BellSound);
-            audio.play();
-        });
     }, [socket]);
 
     return null;
